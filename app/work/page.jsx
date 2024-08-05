bca05b55fd66b0f8e18cbad5d433b55b9f3863b4
Aaron A. Brown <kilonow1275@hotmail.com>
2024-08-05 16:10:03 -0600

    completed contact, work, resume pages w/ styling

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,5 +1,179 @@
+"use client";
+
+// imports
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import WorkSliderBtns from "@/components/ui/WorkSliderBtns";
+
+import { BsFillFolderSymlinkFill, BsDisplay, BsGithub } from "react-icons/bs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from "@/components/ui/tooltip";
+import Link from "next/link";
+import Image from "next/image";
+
+// projects array of objects
+const projects = [
+  {
+    num: "01",
+    category: "frontend",
+    title: "Asset Recovery Firm",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    stack: [
+      { name: "Html5" },
+      { name: "Css3" },
+      { name: "React" },
+      { name: "Javascript" },
+      { name: "NodeJs" },
+      { name: "ExpressJs" },
+      { name: "MongoDb" }
+    ],
+    image: "/assets/work/assetrecoverfirm_proj.png",
+    live: "",
+    github: ""
+  },
+  {
+    num: "02",
+    category: "full-stack",
+    title: "SpotFinder Open Source Project Finder",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    stack: [
+      { name: "Html5" },
+      { name: "Css3" },
+      { name: "React" },
+      { name: "Javascript" },
+      { name: "Redux" },
+      { name: "NodeJs" },
+      { name: "ExpressJs" },
+      { name: "MongoDb" }
+    ],
+    image: "/assets/work/spotfinder_proj.png",
+    live: "",
+    github: ""
+  }
+];
+
 const Work = () => {
-  return <div>Work page</div>;
+  // state
+  const [project, setProject] = useState(projects[0]);
+
+  // handle slider project change
+  const handleSlideChange = (swipe) => {
+    // get current slide index
+    const currentIndex = swipe.activeIndex;
+    // update project state
+    setProject(projects[currentIndex]);
+  };
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" }
+      }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div className="flex flex-col gap-[30px] h-[50%]">
+              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                {project.num}
+              </div>
+              {/*project category */}
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+                {project.category} project
+              </h2>
+              {/*project description */}
+              <p className="text-white/60">{project.description}</p>
+              {/* stack */}
+              <ul className="flex flex-wrap gap-4">
+                {project.stack.map((item, index) => {
+                  return (
+                    <li key={index} className="text-xl text-accent">
+                      {item.name}
+                      {/*remove last comma in array */}
+                      {index !== project.stack.length - 1 && ","}
+                    </li>
+                  );
+                })}
+              </ul>
+              {/*border */}
+              <div className="border border-white/20"></div>
+              {/*buttons */}
+              <div className="flex items-center gap-4">
+                {/*live project button */}
+                <Link href={project.live}>
+                  <TooltipProvider duration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsDisplay className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Live Project</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+                {/*github code button*/}
+                <Link href={project.github}>
+                  <TooltipProvider duration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Github Repository</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%]">
+            <Swiper
+              spaceBetween={30}
+              slidesPerView={1}
+              onSlideChange={handleSlideChange}
+              className="xl:h-[520px] mb-12"
+            >
+              {projects.map((project, index) => {
+                return (
+                  <SwiperSlide key={index} className="w-full">
+                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                      {/*overlay */}
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                      {/*image */}
+                      <div className="relative w-full h-full">
+                        <Image
+                          src={project.image}
+                          fill
+                          alt=""
+                          className="object-cover"
+                        />
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                );
+              })}
+              {/*slide button */}
+              <WorkSliderBtns
+                containerStyles="flex gap-2 absolute right-0 h-[44px] bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] flex justify-center items-center transition-all"
+                iconStyles=""
+              />
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Work;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     "./pages/**/*.{js,jsx}",
     "./components/**/*.{js,jsx}",
     "./app/**/*.{js,jsx}",
-    "./src/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}"
   ],
   prefix: "",
   theme: {
@@ -16,11 +16,11 @@ module.exports = {
         sm: "640px",
         md: "768px",
         lg: "960px",
-        xl: "1200px",
-      },
+        xl: "1200px"
+      }
     },
     fontFamily: {
-      primary: "var(--font-jetbrainsMono)",
+      primary: "var(--font-jetbrainsMono)"
     },
     extend: {
       colors: {
@@ -28,34 +28,34 @@ module.exports = {
         secondary: "#1c1c22",
         accent: {
           DEFAULT: "#00ffff",
-          hover: "#33ffff",
-        },
+          hover: "#00b3b3"
+        }
       },
       keyframes: {
         typing: {
           from: { width: "0" },
-          to: { width: "3.5em" }, // Adjust width as needed
+          to: { width: "3.5em" } // Adjust width as needed
         },
         blink: {
-          "50%": { borderColor: "transparent" },
+          "50%": { borderColor: "transparent" }
         },
 
         "accordion-down": {
           from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "var(--radix-accordion-content-height)" }
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
+          to: { height: "0" }
+        }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         typing: "typing 2s steps(10, end), blink .5s step-end infinite",
-        blink: "blink 1s step-end",
-      },
-    },
+        blink: "blink 1s step-end"
+      }
+    }
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate")]
 };
